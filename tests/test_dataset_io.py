9756fd312f9d3af85b4e063b4b4d5000eeafd807
banesullivan <bane.sullivan@kitware.com>
2022-07-02 12:38:10 -0600

    Add better data conversions from PyVista to xarray

--- tests/test_dataset_io.py
+++ tests/test_dataset_io.py
@@ -5,6 +5,8 @@ import pytest
 import pyvista as pv
 import xarray as xr
 
+from pvxarray import pyvista_to_xarray
+
 
 @pytest.fixture
 def vtr_path():
@@ -49,3 +51,43 @@ def test_read_vts(vts_path):
     assert np.allclose(ds["y"].values, truth.y)
     assert np.allclose(ds["z"].values, truth.z)
     assert ds["Elevation"].pyvista.mesh(x="x", y="y", z="z") == truth
+
+
+def test_convert_vtr(vtr_path):
+    truth = pv.RectilinearGrid(vtr_path)
+    ds = pyvista_to_xarray(truth)
+    mesh = ds["air"].pyvista.mesh(x="x", y="y", z="z")
+    assert np.array_equal(ds["air"].values.ravel(), truth["air"].ravel())
+    assert np.may_share_memory(ds["air"].values.ravel(), truth["air"].ravel())
+    assert np.array_equal(mesh.x, truth.x)
+    assert np.may_share_memory(mesh.x, truth.x)
+    assert np.array_equal(mesh.y, truth.y)
+    assert np.may_share_memory(mesh.y, truth.y)
+    assert np.array_equal(mesh.z, truth.z)
+    assert np.may_share_memory(mesh.z, truth.z)
+    assert mesh == truth
+
+
+def test_convert_vti(vti_path):
+    truth = pv.UniformGrid(vti_path)
+    truth_r = truth.cast_to_rectilinear_grid()
+    ds = pyvista_to_xarray(truth)
+    mesh = ds["RTData"].pyvista.mesh(x="x", y="y", z="z")
+    assert np.array_equal(ds["RTData"].values.ravel(), truth["RTData"].ravel())
+    assert np.may_share_memory(ds["RTData"].values.ravel(), truth["RTData"].ravel())
+    assert np.array_equal(mesh.x, truth_r.x)
+    assert np.array_equal(mesh.y, truth_r.y)
+    assert np.array_equal(mesh.z, truth_r.z)
+    assert mesh == truth_r
+
+
+def test_convert_vts(vts_path):
+    truth = pv.StructuredGrid(vts_path)
+    ds = pyvista_to_xarray(truth)
+    assert np.array_equal(ds["Elevation"].values.ravel(), truth["Elevation"].ravel())
+    assert np.may_share_memory(ds["Elevation"].values.ravel(), truth["Elevation"].ravel())
+    mesh = ds["Elevation"].pyvista.mesh(x="x", y="y", z="z")
+    assert np.array_equal(mesh.x, truth.x)
+    assert np.array_equal(mesh.y, truth.y)
+    assert np.array_equal(mesh.z, truth.z)
+    assert mesh == truth
