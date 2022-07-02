3231ba8ae56424623e78b7f374e9e3ab3de9927f
Bane Sullivan <bane.sullivan@kitware.com>
2022-07-02 12:46:15 -0600

    Adds better PyVista -> xarray conversions (#30)
    * Add better data conversions from PyVista to xarray
    
    * Linting

--- pvxarray/__init__.py
+++ pvxarray/__init__.py
@@ -2,4 +2,5 @@
 from pvxarray._version import __version__
 from pvxarray.accessor import PyVistaAccessor
 from pvxarray.errors import DataCopyWarning
+from pvxarray.io import pyvista_to_xarray
 from pvxarray.report import Report
--- pvxarray/io.py
+++ pvxarray/io.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 
+import numpy as np
 import pyvista as pv
 import xarray as xr
 from xarray.backends import BackendEntrypoint
@@ -24,6 +25,29 @@ def rectilinear_grid_to_dataset(mesh):
     )
 
 
+def image_data_to_dataset(mesh):
+    def gen_coords(i):
+        coords = (
+            np.cumsum(np.insert(np.full(mesh.dimensions[i] - 1, mesh.spacing[i]), 0, 0))
+            + mesh.origin[i]  # noqa: W503
+        )
+        return coords
+
+    dims = list(mesh.dimensions)
+    dims = dims[-1:] + dims[:-1]
+    return xr.Dataset(
+        {
+            name: (["z", "x", "y"], arr.ravel().reshape(dims))
+            for name, arr in mesh.point_data.items()
+        },
+        coords={
+            "x": (["x"], gen_coords(0)),
+            "y": (["y"], gen_coords(1)),
+            "z": (["z"], gen_coords(2)),
+        },
+    )
+
+
 def structured_grid_to_dataset(mesh):
     warnings.warn(
         DataCopyWarning(
@@ -41,6 +65,20 @@ def structured_grid_to_dataset(mesh):
             "z": (["xi", "yi", "zi"], mesh.z),
         },
     )
+
+
+def pyvista_to_xarray(mesh):
+    """Generate an xarray DataSet from a PyVista mesh object."""
+    if isinstance(mesh, pv.RectilinearGrid):
+        return rectilinear_grid_to_dataset(mesh)
+    elif isinstance(mesh, pv.UniformGrid):
+        return image_data_to_dataset(mesh)
+    elif isinstance(mesh, pv.StructuredGrid):
+        return structured_grid_to_dataset(mesh)
+    else:
+        raise TypeError(
+            f"pvxarray is unable to generate an xarray DataSet from the {type(mesh)} VTK/PyVista data type at this time."
+        )
 
 
 class PyVistaBackendEntrypoint(BackendEntrypoint):
@@ -61,16 +99,7 @@ class PyVistaBackendEntrypoint(BackendEntrypoint):
             file_format=file_format,
             progress_bar=progress_bar,
         )
-        if isinstance(mesh, pv.RectilinearGrid):
-            return rectilinear_grid_to_dataset(mesh)
-        elif isinstance(mesh, pv.UniformGrid):
-            return rectilinear_grid_to_dataset(mesh.cast_to_rectilinear_grid())
-        elif isinstance(mesh, pv.StructuredGrid):
-            return structured_grid_to_dataset(mesh)
-        else:
-            raise TypeError(
-                f"pvxarray is unable to generate an xarray DataSet from the {type(mesh)} VTK/PyVista data type."
-            )
+        return pyvista_to_xarray(mesh)
 
     open_dataset_parameters = [
         "filename_or_obj",
