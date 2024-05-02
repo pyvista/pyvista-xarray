2acb1ec2ca4de6458f34a20bd6f290ed5d86ab72
Anne Haley <anne.haley@kitware.com>
2024-05-02 14:09:42 +0000

    style: reformat with black

--- pvxarray/vtk_source.py
+++ pvxarray/vtk_source.py
@@ -263,10 +263,7 @@ time_index: {self._time_index}
             order=self._order,
             component=self._component,
             mesh_type=self._mesh_type,
-            scales = {
-                k: v[2]
-                for k, v in self._slicing.items()
-            }
+            scales={k: v[2] for k, v in self._slicing.items()},
         )
         return self._mesh
 
