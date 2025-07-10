842dd0241263b733956b42f5f2df98f82e80fc96
Bane Sullivan <banesullivan@gmail.com>
2025-07-10 12:36:45 -0700

    Fix pkg_resources deprecation

--- pvxarray/_version.py
+++ pvxarray/_version.py
@@ -1,7 +1,3 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import version
 
-try:
-    __version__ = get_distribution("pyvista-xarray").version
-except DistributionNotFound:  # pragma: no cover
-    # package is not installed
-    __version__ = None
+__version__ = version("pyvista-xarray")
