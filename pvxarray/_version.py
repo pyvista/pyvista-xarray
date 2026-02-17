0201331fdeb766c42acc770136edec5fe2ad66c6
Bane Sullivan <banesullivan@gmail.com>
2026-02-16 20:44:53 -0800

    Update packaging practices

--- pvxarray/_version.py
+++ pvxarray/_version.py
@@ -1,7 +1,7 @@
-from pkg_resources import DistributionNotFound, get_distribution
+from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = get_distribution("pyvista-xarray").version
-except DistributionNotFound:  # pragma: no cover
+    __version__ = version("pyvista-xarray")
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = None
